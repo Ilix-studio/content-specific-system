8861572d995373509e1d5f306a4481e45a3c7828
ilix-studio <ilishjyoti17@gmail.com>
2024-12-20 02:11:46 +0530

    removing required tag to test

--- server/models/newPaymentmodel.js
+++ server/models/newPaymentmodel.js
@@ -4,36 +4,28 @@ const newPaymentSchema = mongoose.Schema(
   {
     razorpay_order_id: {
       type: String,
-      required: true,
     },
     razorpay_payment_id: {
       type: String,
-      required: true,
     },
     razorpay_signature: {
       type: String,
-      required: true,
     },
     instituteId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Institute",
-      required: true,
     },
     courseName: {
       type: String,
-      required: true,
     },
     timePeriod: {
       type: String,
-      required: true,
     },
     passkeyCount: {
       type: Number,
-      required: true,
     },
     amount: {
       type: Number,
-      required: true,
     },
     status: {
       type: String,
